3bc7682c1f9b12cdec2085b5c73cfce81ad1290b
Cindy Bui <ctb335@nyu.edu>
2020-04-22 00:28:54 -0400

    New benchmarking script

--- benchmark.ps1
+++ benchmark.ps1
@@ -4,4 +4,9 @@ $myDIR="LOGS"
 For ($i=6; $i -ge 0; $i--) {
     $VAR=[math]::Pow(10, $i)
     ./Acoustic_Raytracing.out.exe $VAR > $myDIR/$VAR.log
+    if ($VAR -ne 1) {
+        $VAR/=2
+        ./Acoustic_Raytracing.out.exe $VAR > $myDIR/$VAR.log
+    }
+    
 }
